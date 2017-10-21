d446a234e3e8838ac56f3f20470966cf87463469
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-22 01:19:13 +0300

    add base urls and api key

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
-import os
+# import os
 
 class Config:
-    NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?category={}&language=en'
-    NEWS_ARTICLE_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-#  NEWS_API_KEY='3af9695e2c9a47a0827f52a1b4badb83'
+    NEWS_SOURCE_BASE_URL = 'https://newsapi.org/v1/sources?category={}&language=en'
+    # NEWS_ARTICLE_API_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}
+    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    # NEWS_API_KEY='3af9695e2c9a47a0827f52a1b4badb83'
+    pass
 
 class ProdConfig(Config):
     pass
