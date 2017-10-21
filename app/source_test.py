3fc64ad030674a752a6519785ef8a8a537e271e4
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-22 01:21:07 +0300

    add test to confirm that the class in making instances

---
+++ app/source_test.py
@@ -0,0 +1,20 @@
+import unittest
+from models import source
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    testing the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        runs before every test
+        '''
+        self.new_source = Source("mtv-news", "MTV News", "All the latest news from the world of music and entertainment.", "http://www.mtv.co.uk/news", "music")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Source))
+
+if __name__ == '__main__':
+    unittest.main()
