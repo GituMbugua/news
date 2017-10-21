a5f07a63768fffc74214e55e3b619ff06de1d94e
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-22 01:18:38 +0300

    config app and initialize extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
 # from config import config_options
 
-# bootstrap = Bootstrap()
-
 # def create_app(config_name):
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
+
+# initialize flask extensions
+bootstrap = Bootstrap(app)
+
+# set up config
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
