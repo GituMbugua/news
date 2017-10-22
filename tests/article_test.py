2c08180cc95944a9a9d8ad9d26778bc57e5c55b7
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-23 01:34:49 +0300

    restructure directory

--- tests/article_test.py
+++ tests/article_test.py
@@ -1,6 +1,5 @@
 import unittest
-from models import article
-Article = article.Article
+from app.models import Article
 
 class ArticleTest(unittest.TestCase):
     '''
@@ -16,6 +15,3 @@ class ArticleTest(unittest.TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article, Article))
-
-if __name__ == '__main__':
-    unittest.main()
